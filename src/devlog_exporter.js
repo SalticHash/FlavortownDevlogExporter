024c20ebd5deaeb56e92cbbfb992f02fcb4b7094
SalticHash <saltichash@gmail.com>
2026-04-15 11:20:48 -0600

    Updated Manifest, Add Icons, Remove innerHTML vunerable

--- src/devlog_exporter.js
+++ src/devlog_exporter.js
@@ -1,8 +1,3 @@
-// Assets
-const downloadSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" width="512" height="512" fill="none">
-    <path fill="currentColor"
-        d="M352 96C352 78.3 337.7 64 320 64C302.3 64 288 78.3 288 96L288 306.7L246.6 265.3C234.1 252.8 213.8 252.8 201.3 265.3C188.8 277.8 188.8 298.1 201.3 310.6L297.3 406.6C309.8 419.1 330.1 419.1 342.6 406.6L438.6 310.6C451.1 298.1 451.1 277.8 438.6 265.3C426.1 252.8 405.8 252.8 393.3 265.3L352 306.7L352 96zM160 384C124.7 384 96 412.7 96 448L96 480C96 515.3 124.7 544 160 544L480 544C515.3 544 544 515.3 544 480L544 448C544 412.7 515.3 384 480 384L433.1 384L376.5 440.6C345.3 471.8 294.6 471.8 263.4 440.6L206.9 384L160 384zM464 440C477.3 440 488 450.7 488 464C488 477.3 477.3 488 464 488C450.7 488 440 477.3 440 464C440 450.7 450.7 440 464 440z" />
-</svg>`
 
 FTFetch(document.querySelector(".project-card__banner-image").src)
 // Firefox catch-safe
@@ -99,7 +94,11 @@ function createExportButton(showDialog) {
     exportButton.onclick = showDialog
     exportButton.id = "export_button_id"
     exportButton.className = "btn btn--brown btn--borderless"
-    exportButton.innerHTML = `${downloadSVG} Export Devlogs`
+    exportButton.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" width="512" height="512" fill="none">
+        <path fill="currentColor"
+            d="M352 96C352 78.3 337.7 64 320 64C302.3 64 288 78.3 288 96L288 306.7L246.6 265.3C234.1 252.8 213.8 252.8 201.3 265.3C188.8 277.8 188.8 298.1 201.3 310.6L297.3 406.6C309.8 419.1 330.1 419.1 342.6 406.6L438.6 310.6C451.1 298.1 451.1 277.8 438.6 265.3C426.1 252.8 405.8 252.8 393.3 265.3L352 306.7L352 96zM160 384C124.7 384 96 412.7 96 448L96 480C96 515.3 124.7 544 160 544L480 544C515.3 544 544 515.3 544 480L544 448C544 412.7 515.3 384 480 384L433.1 384L376.5 440.6C345.3 471.8 294.6 471.8 263.4 440.6L206.9 384L160 384zM464 440C477.3 440 488 450.7 488 464C488 477.3 477.3 488 464 488C450.7 488 440 477.3 440 464C440 450.7 450.7 440 464 440z" />
+    </svg> Export Devlogs`
     return exportButton
 }
 
@@ -113,7 +112,7 @@ if (!document.getElementById("export_button_id")) {
     const dialog = createDialog()
     const exportButton = createExportButton(() => dialog.showModal())
     const style = document.createElement("style")
-    style.innerHTML = `
+    style.textContent = `
     .modal__actions-close:disabled {
         opacity: 0.6;
         filter: brightness(1.0) !important;
